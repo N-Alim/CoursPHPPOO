e0a105076bad98a7e3c8347d9cbf852ae420261d
Nouira Alim <nouiraalim@gmail.com>
2022-02-11 11:07:39 +0100

    Création de classe Utilisateur

---
+++ classes/Utilisateur.php
@@ -0,0 +1,18 @@
+<?php
+
+class Utilisateur
+{
+    protected string $userName;
+    protected string $userPassword;
+
+    public function __construct(string $name, string $password)
+    {
+        $this->userName  = $name;
+        $this->userPassword  = $password;
+    }
+
+    public function getName()
+    {
+        return $this->userName;
+    }
+}
