167201a163b6f2ee3295636e6b2ef4bab6a06a87
Nouira Alim <nouiraalim@gmail.com>
2022-02-11 11:06:36 +0100

    Test surcharge de méthode

---
+++ utilisateur.php
@@ -0,0 +1,14 @@
+<?php
+date_default_timezone_set("Europe/Paris");
+ini_set('display_errors', '1');
+
+spl_autoload_register(function ($className)
+{
+    include "./classes/" . $className . ".php";
+});
+
+$utilisateur1 = new Administrateur("Michel", "1234");
+
+var_dump($utilisateur1);
+
+echo $utilisateur1->getName();
