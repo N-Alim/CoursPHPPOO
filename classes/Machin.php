2d88ce24fe6b128ec68c8c54e236eaa68226bed9
Nouira Alim <nouiraalim@gmail.com>
2022-02-11 09:55:50 +0100

    Héritage de la classe Test par la classe Machin

---
+++ classes/Machin.php
@@ -0,0 +1,22 @@
+<?php
+
+class Machin extends Test
+{
+    private float $taille;
+
+    public function bonjour($truc) : void
+    {
+        echo "Bonjour $truc";
+    }
+
+    public function getTaille() : float
+    {
+        return $this->taille;
+    }
+
+    public function setTaille(float $combien) : void
+    {
+        $this->taille = $combien;
+    }
+
+}
