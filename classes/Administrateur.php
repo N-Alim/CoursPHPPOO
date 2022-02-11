c40914893583086fd04d9d3080dd136015cb3145
Nouira Alim <nouiraalim@gmail.com>
2022-02-11 12:44:23 +0100

    Création d'une interface

--- classes/Administrateur.php
+++ classes/Administrateur.php
@@ -1,11 +1,16 @@
 <?php
 
-class Administrateur extends Utilisateur
+class Administrateur extends Utilisateur implements NouvelUtilisateur
 {
     public function getName()
     {
         echo parent::getName();
         echo "cela marche";
+    }
+
+    public function effectuerPeriodeEssai()
+    {
+        echo "Je suis en période d'essai";
     }
 
     public function getRole()
